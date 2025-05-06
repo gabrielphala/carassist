aaf8b3ff5845a190115c98e12c2f7f87b8c2e089
Gabriel Phala <gabrielphala@outlook.com>
2025-05-06 17:45:12 +0200

    db live url

--- node/src/config/database.ts
+++ node/src/config/database.ts
@@ -1,7 +1,11 @@
 import { connect } from "mongoose"
 
 export default async () => {
-    await connect(decodeURIComponent(process.env.DATABASE_URL))
+    await connect(decodeURIComponent(
+        ["development", 'staging'].includes(process.env.NODE_ENV)
+        ? process.env.DATABASE_URL
+        : process.env.CLOUD_DATABASE_URL
+    ))
 }
 
 export const MONGO_ID_LEN = 24;
